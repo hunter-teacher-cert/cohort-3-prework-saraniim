3cb229c6c6efeab9e506e5f86ee1e1bc262d8393
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-01-29 17:53:57 +0000

    Added Date assignment

---
+++ pre02/Date.java
@@ -0,0 +1,20 @@
+
+
+ class Date{
+  public static void main(String[] args) {
+        String firstLine = "Date Check!";
+        System.out.println(firstLine);
+
+        String day = "Saturday";
+        String month = "January";
+        int date = 29;
+        int year = 2022;
+
+        System.out.println("American format:");
+        System.out.println(day + "," + month + " " + date + "," + year);
+        System.out.println("European format:");
+        System.out.println(day + " " + date + " " + month + " " + year);
+
+
+}
+ }
