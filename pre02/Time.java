65c4ae92bafca05eabda37fa718c20889add765b
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-02-01 17:16:50 +0000

    added individual

--- pre02/Time.java
+++ pre02/Time.java
@@ -1,19 +1,12 @@
-
-
  class Date{
   public static void main(String[] args) {
         String firstLine = "Date Check!";
         System.out.println(firstLine);
 
-        String day = "Saturday";
-        String month = "January";
+        int hour = 11
+        int minute = 29
         int date = 29;
         int year = 2022;
-
-        System.out.println(day);
-        System.out.println(month);
-        System.out.println(date);
-        System.out.println(year);
 
         System.out.println("American format:");
         System.out.println(day + "," + month + " " + date + "," + year);
