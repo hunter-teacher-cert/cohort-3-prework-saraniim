5da3dc3659c364cb309085137469ce24671fda66
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-02-01 18:23:11 +0000

    Time.java

--- pre02/Time.java
+++ pre02/Time.java
@@ -1,18 +1,40 @@
- class Date{
-  public static void main(String[] args) {
-        String firstLine = "Date Check!";
+ class Time{
+
+    public static void main(String[] args) {
+        String firstLine = "Time Warp!";
         System.out.println(firstLine);
 
-        int hour = 11
-        int minute = 29
-        int date = 29;
-        int year = 2022;
+      // Exercise 3 part #2
+        int hour = 12;
+        int minute = 59;
+        int second = 24;
+         
+        System.out.print("The current time is ");
+        System.out.print(hour);
+        System.out.print(":");
+        System.out.print(minute);
+        System.out.println(".");
 
-        System.out.println("American format:");
-        System.out.println(day + "," + month + " " + date + "," + year);
-        System.out.println("European format:");
-        System.out.println(day + " " + date + " " + month + " " + year);
+        //Exercise 3 Part 3
 
+        System.out.print("Number of seconds since midnight: ");
+        System.out.println((hour * 60 * 60) + (minute * 60) + second);
+
+      //Exercise 3 part 4
+        System.out.print("Seconds remaining of the day: ");
+        System.out.println((24 * 60 * 60) -  ((hour * 60 * 60) + (minute * 60) + second));
+      //Exercise 3 Part 5
+        System.out.print("Percent of the day that has passed: ");
+        System.out.println(((hour * 60 * 60) + (minute * 60) + second) * 100/ (24 * 60 * 60));
+
+      //Exercise 3 Part 6 - Elapsed time
+      // original time was 12:29:24
+        System.out.print("Elapsed time: ");
+        System.out.println((hour * 60 + minute) - (12 * 60 + 29) );  
+
+
+
+
+    }
 
 }
- }
