0aa953c52c3cbbf5a4f46816e95e497869338b60
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-02-06 00:20:56 +0000

    Added comment code

--- pre02/Time.java
+++ pre02/Time.java
@@ -5,17 +5,24 @@
         System.out.println(firstLine);
 
       // Exercise 3 part #2
+      //    current hour
         int curhour;
         curhour = 12;
+        // current minute
         int curminute;
         curminute = 59;
+        //current seconds
         int cursecond;
         cursecond = 24;
 
+        //  seconds per hour
         int hoursec;
         hoursec = 360;
+        // minutes per hour
         int hourmin;
         hourmin = 60;
+
+        //seconds per minute
         int minsec;
         minsec = 60;
          
