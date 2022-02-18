2c884572102697664c0ad724abc6d424b52f6070
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-02-18 21:17:35 +0000

    Celsius.java

---
+++ pre02/Celsius.java
@@ -0,0 +1,20 @@
+// allows for user input
+import java.util.Scanner;
+
+public class Celsius {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        // Input temperature in Celsius from user 
+        System.out.print("Enter temperature in Celsius: ");
+        double cel = in.nextDouble();
+        
+        // Convert Celsius to Fahrenheit
+        double F = cel * (9.0 / 5.0) + 32;
+        
+        // Print result
+        System.out.println(cel + " Celsius is " + F +" degrees Fahrenheit.");
+       
+    }
+}
