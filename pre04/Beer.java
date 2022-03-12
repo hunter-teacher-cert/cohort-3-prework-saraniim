de2ef9ced81c61dcccd8a4e4bb57c644abff5377
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-03-12 19:35:40 +0000

    beer

---
+++ pre04/Beer.java
@@ -0,0 +1,26 @@
+public class Beer {
+public static void main(String[] args) {
+    b(99);
+
+}
+
+public static void b(int n) { 
+    if (n == 0){
+        System.out.println();
+        System.out.println("No bottles of beer on the wall,");
+        System.out.println("no bottles of beer,");
+        System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
+        System.out.println("’cause there are no more bottles of beer on the wall!");
+    } 
+    else{
+        System.out.println(n + " bottles of beer on the wall,");
+        System.out.println(n + " bottles of beer,");
+        System.out.println("Ya' take one down, ya' pass it around,");
+        System.out.println((n-1) + " bottles of beer on the wall");
+        System.out.println();
+
+        b(n - 1);
+
+    }
+} 
+}
