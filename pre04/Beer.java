e3ed4b8e7cf14d046dbf7669f7170ca0adc41eca
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-03-12 19:46:19 +0000

    carriage return

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -7,16 +7,10 @@ public static void main(String[] args) {
 public static void b(int n) { 
     if (n == 0){
         System.out.println();
-        System.out.println("No bottles of beer on the wall,");
-        System.out.println("no bottles of beer,");
-        System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
-        System.out.println("’cause there are no more bottles of beer on the wall!");
+        System.out.println("No bottles of beer on the wall,\n no bottles of beer,\n ya’ can’t take one down,\n ya’ can’t pass it around,\n ’cause there are no more bottles of beer on the wall!");
     } 
     else{
-        System.out.println(n + " bottles of beer on the wall,");
-        System.out.println(n + " bottles of beer,");
-        System.out.println("Ya' take one down, ya' pass it around,");
-        System.out.println((n-1) + " bottles of beer on the wall");
+        System.out.println(n + " bottles of beer on the wall,\n" +  (n) + " bottles of beer,\n Ya' take one down, ya' pass it around,\n" + (n-1) + " bottles of beer on the wall");
         System.out.println();
 
         b(n - 1);
