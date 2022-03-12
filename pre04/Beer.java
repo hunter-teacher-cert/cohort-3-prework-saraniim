a7ed6d559ab7042213c8a547ee25135dd4a18a9a
Saranii Muller <56198179+saraniim@users.noreply.github.com>
2022-03-12 19:49:36 +0000

    spaces with carriage returns

--- pre04/Beer.java
+++ pre04/Beer.java
@@ -7,10 +7,10 @@ public static void main(String[] args) {
 public static void b(int n) { 
     if (n == 0){
         System.out.println();
-        System.out.println("No bottles of beer on the wall,\n no bottles of beer,\n ya’ can’t take one down,\n ya’ can’t pass it around,\n ’cause there are no more bottles of beer on the wall!");
+        System.out.println("No bottles of beer on the wall,\nno bottles of beer,\nya’ can’t take one down,\nya’ can’t pass it around,\n’cause there are no more bottles of beer on the wall!");
     } 
     else{
-        System.out.println(n + " bottles of beer on the wall,\n" +  (n) + " bottles of beer,\n Ya' take one down, ya' pass it around,\n" + (n-1) + " bottles of beer on the wall");
+        System.out.println(n + " bottles of beer on the wall,\n" + (n) +" bottles of beer,\nYa' take one down, ya' pass it around,\n" + (n-1) +" bottles of beer on the wall");
         System.out.println();
 
         b(n - 1);
